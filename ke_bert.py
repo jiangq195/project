1b01119e67528bef28cb9e72755c9ea2e2f8d6e9
蒋强 <jiangq113@163.com>
2020-06-24 13:42:21 +0800

    keras_bert

---
+++ ke_bert.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import codecs, gc
+from sklearn.model_selection import KFold
+from keras_bert import load_trained_model_from_checkpoint, Tokenizer
+from keras.metrics import top_k_categorical_accuracy
+from keras.layers import *
+from keras.callbacks import *
+from keras.models import Model
+import keras.backend as K
+from keras.optimizers import K
